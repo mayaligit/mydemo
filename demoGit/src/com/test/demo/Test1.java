a94ec0189864f4d5b6db3a5960f630ff700f274d
mayali <mayaliyx@163.com>
2017-11-24 18:09:22 +0800

    111

--- demoGit/src/com/test/demo/Test1.java
+++ demoGit/src/com/test/demo/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("很久没有用了，甚是想念那");
+		System.out.println("很久没有用了，甚是想念那");
+		
+		System.out.println("哈哈哈哈哈哈哈哈哈哈");
+		System.out.println("哈哈哈哈哈哈哈哈哈哈");
+		System.out.println("哈哈哈哈哈哈哈哈哈哈");
 	}
 
 }
