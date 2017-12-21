3214a7a622f716473f9f5da80388e21beecb6d8b
chenzhihao <uiuu@163.com>
2017-12-21 11:17:33 +0800

    考勤统计优化   11：17

--- attendances/src/main/java/com/cmic/attendance/service/WorkStatisticsService.java
+++ attendances/src/main/java/com/cmic/attendance/service/WorkStatisticsService.java
@@ -190,7 +190,7 @@ public class WorkStatisticsService extends CrudService<WorkStatisticsDao, WorkSt
         //SimpleDateFormat strdate = new SimpleDateFormat("EEEE");
         SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("yyyy-MM");
         SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("yyyy-MM-dd ");
-        //SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-d HH:mm");
+        SimpleDateFormat simpleDateFormat5 = new SimpleDateFormat("yyyy年MM月d日 ");
         SimpleDateFormat simpleDateFormat4 = new SimpleDateFormat(" HH:mm");
 
         List<Attendance> attendanceDays = attendanceDao.getAttendanceDays(workStatistics);//某个月的出勤
@@ -273,7 +273,7 @@ public class WorkStatisticsService extends CrudService<WorkStatisticsDao, WorkSt
         }
         for (Integer m : miss) {//封装旷工详情
             calendar2.set(Calendar.DAY_OF_MONTH, m);
-            AbsenteeismData.add(simpleDateFormat3.format(calendar2.getTime()) + strdate.format(calendar2.getTime()));
+            AbsenteeismData.add(simpleDateFormat5.format(calendar2.getTime()) + strdate.format(calendar2.getTime()));
         }
         //组装 map 返回
         double holidayDays_sum = 0;//请假总时长
